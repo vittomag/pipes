7ea382a71c3f91852d87c184c4507366f78f1a1f
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-09-26 12:20:04 -0300

    Save Filter

---
+++ src/Library/Filters/SaveFilter.cs
@@ -0,0 +1,19 @@
+﻿namespace CompAndDel.Filters;
+using System;
+using CompAndDel;
+
+public class SaveFilter : IFilter
+{
+    private PictureProvider p = new PictureProvider();
+    public string Path { get; set; }
+
+    public SaveFilter(string path)
+    {
+        this.Path = path;
+    }
+    public IPicture Filter(IPicture result)
+    {
+        p.SavePicture(result, this.Path );
+        return result;
+    }
+}
